8e78db85e781f6e3e366588060c480cb9f529c32
Amy <cmephro@aol.com>
2015-01-12 12:53:02 -0600

    js attempt

--- js/app.js
+++ js/app.js
@@ -35,8 +35,8 @@ function app() {
                 console.log("request succeeded");
             });
 
-            model.fetch().then(function(mod){
-            	console.log(mod.toJSON()); 
+            model.fetch().then(function(mod) {
+                console.log(mod.toJSON());
             });
 
             this.remove();
@@ -45,27 +45,56 @@ function app() {
     });
 
     window.GeneologyFormModel = Parse.Object.extend('FormSubmission', {
-        defaults: {
-        	permission: "off",
-            first_name: "jhmomc",
-            last_name: "JHMOMC",
-            arrived_america: 2014,
-            arrived_mc: 2014,
-            b_mitzvah: "NA",
-            bagel: "NA",
-            bakery: "NA"
+        // defaults: {
+        //     permission: "off",
+        //     first_name: "jhmomc",
+        //     last_name: "JHMOMC",
+        //     arrived_america: 2014,
+        //     arrived_mc: 2014,
+        //     b_mitzvah: "NA",
+        //     bagel: "NA",
+        //     bakery: "NA"
 
-        },
-        validate: function(attrs, options){
-        	if (attrs.permission !== "on"){
-        		return "you must check the box to continue";
-        	}
-        }
+        // },
+        // validate: function(attrs, options) {
+        //     if (attrs.permission !== "on") {
+        //         return "you must check the box to continue";
+        //     }
+        // }
     });
+
 
     var myFormView = new GeneologyFormView();
     // console.log(myFormView)
 
+    // //WAT section
+
+    // var Data = Parse.Object.extend('data', {});
+    // //^ should be same as model
+    // // var myModel = new GeneologyFormModel(object);
+    // // myModel.fetch().then(function(mod) {
+    // //     console.log(mod.toJSON());
+    // // });
+
+
+    // var List = Parse.Collection.extend({
+    //     // model: myModel
+    //     model: Data
+    // });
+    // var myList = new List();
+    // console.log(myList.toJSON());
+    // // console.log(Data);
+    // myList.fetch().then(function(item) {
+    //     console.log(item.toJSON());
+    // })
+    // var lastName = "Troppe";
+    // myList.fetch().then(function() {
+    //     var matching = myList.filter(function(r) {
+    //         // return r.attributes.last_name === lastName;
+    //         return r.attributes.last_name;
+    //     });
+    //     console.log(matching);
+    // });
 
 
 
