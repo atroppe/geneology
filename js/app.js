fd02eb384cf736c92bded72fe97d0b11ee1ce884
Amy <cmephro@aol.com>
2015-01-19 14:27:17 -0600

    adding middle name and remove unneeded files

--- js/app.js
+++ js/app.js
@@ -3,26 +3,16 @@ window.onload = app;
 
 function app() {
 
-    // Parse.initialize("YDmkliixtm3UwCGsRprwcZU2sQPrSjYjDqsEaxRI", "wukv1OaeNM2gcW1hvmHZpgkW6TUQk8n5vHKU62Tt");
     Parse.initialize("7ILyarTpCO2nWbjt1mxeB2iCOdiIdXvdXJkQmiua", "cLf5hm7uCoDQ2JvHY2gNkvPc8t6MSqaVYCVBp5u5");
-    // var TestObject = Parse.Object.extend("TestObject");
-    // var testObject = new TestObject();
-    // testObject.save({
-    //     foo: "bar"
-    // }).then(function(object) {
-    //     alert("yay! it worked");
-    // });
 
 
     window.GeneologyFormView = Parse.View.extend({
-        el: document.querySelector("form"),
+        el: document.querySelector("form#newEntry"),
         events: {
             "submit": "handleFormSubmit"
         },
         handleFormSubmit: function(event) {
-            event.preventDefault(); // don't refresh page (server-side app)
-
-            // get the info in the form
+            event.preventDefault(); 
             var arrayOfObjects = this.$el.find(":input").serializeArray();
             var object = {};
 
@@ -31,8 +21,9 @@ function app() {
             })
 
             var model = new GeneologyFormModel(object);
+
             model.save().then(function() {
-                console.log("request succeeded");
+                console.log("data saved");
             });
 
             model.fetch().then(function(mod) {
@@ -46,26 +37,33 @@ function app() {
 
     window.GeneologyFormModel = Parse.Object.extend('FormSubmission', {
         // defaults: {
-        //     permission: "off",
-        //     first_name: "jhmomc",
-        //     last_name: "JHMOMC",
-        //     arrived_america: 2014,
-        //     arrived_mc: 2014,
-        //     b_mitzvah: "NA",
-        //     bagel: "NA",
-        //     bakery: "NA"
-
+        //     'permission': "",
+        //     'email': "",
+        //     'first_name': "",
+        //     'last_name': "",
         // },
         // validate: function(attrs, options) {
-        //     if (attrs.permission !== "on") {
-        //         return "you must check the box to continue";
+        //     console.log(attrs.email);
+        //     if (!attrs.permission) {
+        //         return "You must check the box to continue.";
+        //     }
+        //     if (!attrs.email) {
+        //         return "Please enter your email."
+        //     }
+        //     if (!attrs.first_name) {
+        //         return "Please enter your first name.";
+        //     }
+        //     if (!attrs.last_name) {
+        //         return "Please enter your last name."
         //     }
         // }
     });
 
 
     var myFormView = new GeneologyFormView();
-    // console.log(myFormView)
+
+
+
     var headerHeight = $('header').height();
 
     function smoothScroll(name) {
@@ -109,41 +107,6 @@ function app() {
     clickScroll('mother');
     clickScroll('m-gf');
     clickScroll('m-gm');
-
-
-
-
-
-    // //WAT section
-
-    // var Data = Parse.Object.extend('data', {});
-    // //^ should be same as model
-    // // var myModel = new GeneologyFormModel(object);
-    // // myModel.fetch().then(function(mod) {
-    // //     console.log(mod.toJSON());
-    // // });
-
-
-    // var List = Parse.Collection.extend({
-    //     // model: myModel
-    //     model: Data
-    // });
-    // var myList = new List();
-    // console.log(myList.toJSON());
-    // // console.log(Data);
-    // myList.fetch().then(function(item) {
-    //     console.log(item.toJSON());
-    // })
-    // var lastName = "Troppe";
-    // myList.fetch().then(function() {
-    //     var matching = myList.filter(function(r) {
-    //         // return r.attributes.last_name === lastName;
-    //         return r.attributes.last_name;
-    //     });
-    //     console.log(matching);
-    // });
-
-
 
 
 }
